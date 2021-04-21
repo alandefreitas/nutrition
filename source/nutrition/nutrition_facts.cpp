e807c0501178f0c0c300fe9763c5bbfac301990a
glauberss2007 <glauberss2007@gmail.com>
2021-04-21 07:38:02 -0300

    implemented no negative result for operator-=

--- source/nutrition/nutrition_facts.cpp
+++ source/nutrition/nutrition_facts.cpp
@@ -112,45 +112,76 @@ namespace nutrition {
 
 
     nutrition_facts& nutrition_facts::operator-=(const nutrition_facts& other) {
-        // General
-        calories -= other.calories;
-
         // Serving size (in grams)
         serving_weight -= other.serving_weight;
+        if(serving_weight<0){
+            *this = *this*0;
+            return *this;
+        } else{
 
-        // Basic Facts
-        protein -= other.protein;
-        fat -= other.fat;
-        carbohydrates -= other.carbohydrates;
-        sugars -= other.sugars;
-        fiber -= other.fiber;
-        cholesterol -= other.cholesterol;
-        saturated_fats -= other.saturated_fats;
+            // General
+            calories -= other.calories;
+            if (calories < 0) calories=0;
 
-        // Vitamins
-        calcium -= other.calcium;
-        iron -= other.iron;
-        potassium -= other.potassium;
-        magnesium -= other.magnesium;
-        vitamin_a -= other.vitamin_a;
-        vitamin_c -= other.vitamin_c;
-        vitamin_b_12 -= other.vitamin_b_12;
-        vitamin_d -= other.vitamin_d;
-        vitamin_e -= other.vitamin_e;
-        omega_3 -= other.omega_3;
-        omega_6 -= other.omega_6;
-        lactose -= other.lactose;
-        phosphorus -= other.phosphorus;
-        sodium -= other.sodium;
-        zinc -= other.zinc;
-        copper -= other.copper;
-        manganese -= other.manganese;
-        selenium -= other.selenium;
-        carotene -= other.carotene;
+            // Basic Facts
+            protein -= other.protein;
+            if (protein < 0) protein=0;
+            fat -= other.fat;
+            if (fat < 0) fat=0;
+            carbohydrates -= other.carbohydrates;
+            if (carbohydrates < 0) carbohydrates=0;
+            sugars -= other.sugars;
+            if (sugars < 0) sugars=0;
+            fiber -= other.fiber;
+            if (fiber < 0) fiber=0;
+            cholesterol -= other.cholesterol;
+            if (cholesterol < 0) cholesterol=0;
+            saturated_fats -= other.saturated_fats;
+            if (saturated_fats < 0) saturated_fats=0;
+
+            // Vitamins
+            calcium -= other.calcium;
+            if (calcium < 0) calcium=0;
+            iron -= other.iron;
+            if (iron < 0) iron=0;
+            potassium -= other.potassium;
+            if (potassium < 0) potassium=0;
+            magnesium -= other.magnesium;
+            if (magnesium < 0) magnesium=0;
+            vitamin_a -= other.vitamin_a;
+            if (vitamin_a < 0) vitamin_a=0;
+            vitamin_c -= other.vitamin_c;
+            if (vitamin_c < 0) vitamin_c=0;
+            vitamin_b_12 -= other.vitamin_b_12;
+            if (vitamin_b_12 < 0) vitamin_b_12=0;
+            vitamin_d -= other.vitamin_d;
+            if (vitamin_d < 0) vitamin_d=0;
+            vitamin_e -= other.vitamin_e;
+            if (vitamin_e < 0) vitamin_e=0;
+            omega_3 -= other.omega_3;
+            if (omega_3 < 0) omega_3=0;
+            omega_6 -= other.omega_6;
+            if (omega_6 < 0) omega_6=0;
+            lactose -= other.lactose;
+            if (lactose < 0) lactose=0;
+            phosphorus -= other.phosphorus;
+            if (phosphorus < 0) phosphorus=0;
+            sodium -= other.sodium;
+            if (sodium < 0) sodium=0;
+            zinc -= other.zinc;
+            if (zinc < 0) zinc=0;
+            copper -= other.copper;
+            if (copper < 0) copper=0;
+            manganese -= other.manganese;
+            if (manganese < 0) manganese=0;
+            selenium -= other.selenium;
+            if (selenium < 0) selenium=0;
+            carotene -= other.carotene;
+            if (carotene < 0) carotene=0;
+        }
 
         return *this;
     }
-
 
     nutrition_facts& nutrition_facts::operator*=(const nutrition_facts& other) {
         // General
