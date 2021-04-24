16869298f7e5d11d960aa88d7fa7773a1a23ac3e
glaubers <glauberss2007@gmail.com>
2021-04-24 13:21:14 -0300

    start to implement food-database unit tests

---
+++ test/unit_test/test_food_database.cpp
@@ -0,0 +1,17 @@
+//
+// Created by glaubers on 23/04/2021.
+//
+
+#define CATCH_CONFIG_MAIN
+
+#include "catch.hpp"
+#include "nutrition/food_database.h"
+
+
+nutrition::food_database tfd;
+
+/*
+TEST_CASE( "Load database" ) {
+    REQUIRE(tfd.size() > 1000);
+}
+*/
