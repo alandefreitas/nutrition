a8154e80249d814c02aa9d90e5e1796ff5b45358
glauberss2007@gmail.com <$6qgQdca>
2021-05-13 10:27:11 -0300

    Removed redundant code

--- source/nutrition/food_database.cpp
+++ source/nutrition/food_database.cpp
@@ -28,7 +28,7 @@ namespace nutrition {
                 tmp.name = row["Name"].get<std::string>();
 
                 auto double_or_zero = [](csv::CSVField field) -> double {
-                  if (field.is_float() || field.is_int() || field.is_num()) {
+                  if (field.is_num()) {
                       return field.get<double>();
                   } else {
                       return 0.0;
